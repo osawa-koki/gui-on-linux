dfa7d3bb0e3f4c928a1b0e1781681e1f05679f12
osawa-koki <github@osawa.dev>
2022-11-21 03:34:08 +0900

    グループ取得に関する変更。

--- server/app/parser/group_get.py
+++ server/app/parser/group_get.py
@@ -4,10 +4,11 @@ def group_get(stdio):
     result = []
     for line in lines:
         line = line.split(':')
+        users = line[3].split(',')
         result.append({
             'name': line[0],
             'password': line[1],
             'gid': line[2],
-            'members': line[3]
+            'members': users
         })
     return result
