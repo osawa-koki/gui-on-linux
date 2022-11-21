bd4ea00af0e806531376599951da90a5d86d9292
osawa-koki <github@osawa.dev>
2022-11-21 22:28:54 +0900

    404 Not Found

--- client/src/Pages/root.tsx
+++ client/src/Pages/root.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import file_img from '../Assets/file.png';
 import user_img from '../Assets/user.png';
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,16 @@
+// React全般設定
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+// CSS設定
 import './index.scss';
+
+// ページ設定
 import App from './App';
 import Root from './Pages/root';
-import reportWebVitals from './reportWebVitals';
+import NotFound from './Pages/notFound';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,6 +21,7 @@ root.render(
       <Routes>
         <Route path="/" element={<Root />} />
         <Route path="/app" element={<App />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
