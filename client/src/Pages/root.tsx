a2b57e1f45c6ddbf1226d43689bad95d68207e06
osawa-koki <github@osawa.dev>
2022-11-21 21:48:27 +0900

    file

--- client/src/Pages/root.tsx
+++ client/src/Pages/root.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import file_img from '../Assets/file.png';
 import './root.scss';
 
 function Root() {
   return (
     <div className="Root">
-      <Link to="/app">Contact</Link>
-      <Link to="/app">Contact</Link>
-      <Link to="/app">Contact</Link>
+      <Link to="/file">
+        <img src={file_img} alt="file" />
+        <div className='description'></div>
+      </Link>
+      <Link to="/user">
+        <img src="" alt="" />
+        <div className='description'></div>
+      </Link>
+      <Link to="/group">
+        <img src="" alt="" />
+        <div className='description'></div>
+      </Link>
+      <Link to="/process">
+        <img src="" alt="" />
+        <div className='description'></div>
+      </Link>
+      <Link to="/net">
+        <img src="" alt="" />
+        <div className='description'></div>
+      </Link>
     </div>
   );
 }
