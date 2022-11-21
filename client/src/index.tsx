cf3ef2ec7cd694afc5ef1f6f9dea63336b19be41
osawa-koki <github@osawa.dev>
2022-11-21 21:16:52 +0900

    scss化した。

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import Index from './Pages/Index';
 import reportWebVitals from './reportWebVitals';
